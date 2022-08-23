41b3006ebb1a76c6ad081866ee87b4521c0b4a5e
gdineshkumarcse <gdineshkumarcse@gmail.com>
2022-08-23 19:26:00 +0530

    slightly changes

--- src/main/java/com/example/sms/service/StudentService.java
+++ src/main/java/com/example/sms/service/StudentService.java
@@ -2,8 +2,18 @@ package com.example.sms.service;
 
 import java.util.List;
 
+
+
 import com.example.sms.entity.Student;
 
 public interface StudentService {
 	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	Student updateStudent(Student student);
+	
+	void deleteStudentByStudentId(Long id);
+	
 }
